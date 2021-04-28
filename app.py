e32e73d45bd162282deb9230b3c5833086a98d8a
mikhail-larionov <mynote02012014@gmail.com>
2021-04-28 10:22:07 +0300

    upd

--- app.py
+++ app.py
@@ -6,26 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def main():
-    return render_template('login.html')
+    return render_template('entry.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'Username - ' + name + ', user id - ' + str(id)
-
-
-@app.route('/data')
-def data():
-    return render_template('data.html')
-
-
-@app.route('/find', methods=['post', 'get'])
-def login():
+@app.route('/find', methods=['post'])
+def calculate():
     summa = request.form['summa']
     percents = request.form['percents']
     month = request.form['month']
-    title = 'Here are your results'
-    amount_per_month = get_amount_of_payment_per_month(summa, month, percents) #str(phrase)
+    title = 'Ваши результаты'
+    amount_per_month = get_amount_of_payment_per_month(summa, month, percents)
     amount_per_all_period = get_amount_per_all_period(amount_per_month, month)
     return render_template('results.html', the_title=title,
                            summma=summa, percents=percents,
@@ -33,28 +23,13 @@ def login():
 
 
 def get_amount_per_all_period(amount_per_month, month):
-    return math.ceil(amount_per_month * int(month))
+    return int(amount_per_month * int(month))
 
 
 def get_amount_of_payment_per_month(summa, month, percent):
-    r = int(percent) / (100 * int(month))
+    r = int(percent) / 1200
     result = int(summa) * r * pow((1 + r), int(month)) / (pow((1 + r), int(month)) - 1)
-    return math.ceil(result)
-
-
-    # message = ''
-    # username = None
-    # password = None
-    # if request.method == 'POST':
-    #     username = request.form.get('username')  # запрос к данным формы
-    #     password = request.form.get('password')
-    #     if username == 'root' and password == 'pass':
-    #         message = "Correct username and password"
-    #     else:
-    #         message = "Wrong username or password"
-    # print('tut')
-    #
-    # return render_template('login.html', message=message)
+    return int(result)
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,9 +0,0 @@
-summa = 20000
-r = 12/100/12
-month = 36
-
-
-pay = summa * r * pow((1 + r), month) / (pow((1 + r), month) - 1)
-print(pay)
-print(pay * month)
-
